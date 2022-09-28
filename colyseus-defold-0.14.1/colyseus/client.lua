864a78fc3c3a6ddf329c6f2ea4e168890b0bd5a6
COCO <support@cocosgames.com>
2022-09-28 12:54:16 +0800

    client / eventemitter / room

--- colyseus-defold-0.14.1/colyseus/client.lua
+++ colyseus-defold-0.14.1/colyseus/client.lua
@@ -1,142 +1,57 @@
-local Connection = require('colyseus.connection')
-local Auth = require('colyseus.auth')
-local Room = require('colyseus.room')
-local Push = require('colyseus.push')
-local protocol = require('colyseus.protocol')
-local EventEmitter = require('colyseus.eventemitter')
-local storage = require('colyseus.storage')
 
-local utils = require('colyseus.utils')
-local decode = require('colyseus.serialization.schema.schema')
-local JSON = require('colyseus.serialization.json')
-local msgpack = require('colyseus.messagepack.MessagePack')
-
+---@class Client
 local client = {}
-client.__index = client
 
-function client.new (endpoint)
-  local instance = EventEmitter:new()
-  setmetatable(instance, client)
-  instance:init(endpoint)
-  return instance
-end
+---Instantiate a colyseus client.
+---@param endpoint string
+---@return Client
+function client.new (endpoint) end
 
-function client:init(endpoint)
-  self.hostname = endpoint
+---Queries for all available rooms to connect.
+---@param room_name string
+---@param callback fun(err:string,rooms:table):nil
+---@return nil
+function client:get_available_rooms(room_name, callback) end
 
-  -- ensure the ends with "/", to concat with path during create_connection.
-  if string.sub(self.hostname, -1) ~= "/" then
-    self.hostname = self.hostname .. "/"
-  end
+---Join an existing room or create a new one.
+---@param room_name string
+---@param options table
+---@param callback fun(err:string,room:Room):nil
+---@return nil
+function client:join_or_create(room_name, options, callback) end
 
-  self.auth = Auth.new(endpoint)
-  self.push = Push.new(endpoint)
+---Creates a new room.
+---@param room_name string
+---@param options table
+---@param callback fun(err:string,room:Room):nil
+---@return nil
+function client:create(room_name, options, callback) end
 
-  self.rooms = {}
-end
+---Joins an existing room.
+---@param room_name string
+---@param options table
+---@param callback fun(err:string,room:Room):nil
+---@return nil
+function client:join(room_name, options, callback) end
 
-function client:get_available_rooms(room_name, callback)
-  local url = "http" .. self.hostname:sub(3) .. "matchmake/" .. room_name
-  local headers = { ['Accept'] = 'application/json' }
-  self:_request(url, 'GET', headers, nil, callback)
-end
+---Joins an existing room by its roomId.
+---@param room_id string
+---@param options table
+---@param callback fun(err:string,room:Room):nil
+---@return nil
+function client:join_by_id(room_id, options, callback) end
 
-function client:join_or_create(room_name, options, callback)
-  return self:create_matchmake_request('joinOrCreate', room_name, options or {}, callback)
-end
+---Reconnects the client into a room he was previously connected with.
+---@param room_id string
+---@param session_id string
+---@param callback fun(err:string,room:Room):nil
+---@return nil
+function client:reconnect(room_id, session_id, callback) end
 
-function client:create(room_name, options, callback)
-  return self:create_matchmake_request('create', room_name, options or {}, callback)
-end
-
-function client:join(room_name, options, callback)
-  return self:create_matchmake_request('join', room_name, options or {}, callback)
-end
-
-function client:join_by_id(room_id, options, callback)
-  return self:create_matchmake_request('joinById', room_id, options or {}, callback)
-end
-
-function client:reconnect(room_id, session_id, callback)
-  return self:create_matchmake_request('joinById', room_id, { sessionId = session_id }, callback)
-end
-
-function client:create_matchmake_request(method, room_name, options, callback)
-  if type(options) == "function" then
-    callback = options
-    options = {}
-  end
-
-  if self.auth:has_token() then
-    options.token = self.auth.token
-  end
-
-  local headers = {
-    ['Accept'] = 'application/json',
-    ['Content-Type'] = 'application/json'
-  }
-
-  local url = "http" .. self.hostname:sub(3) .. "matchmake/" .. method .. "/" .. room_name
-  self:_request(url, 'POST', headers, JSON.encode(options), function(err, response)
-    if (err) then return callback(err) end
-
-    self:consume_seat_reservation(response, callback)
-  end)
-end
-
-function client:consume_seat_reservation(response, callback)
-  local room = Room.new(response.room.name)
-  room.id = response.room.roomId
-  room.sessionId = response.sessionId
-
-  local on_error = function(err)
-    callback(err, nil)
-    room:off()
-  end
-
-  local on_join = function()
-    room:off('error', on_error)
-    callback(nil, room)
-  end
-
-  room:on('error', on_error)
-  room:on('join', on_join)
-  room:on('leave', function()
-    self.rooms[room.id] = nil
-  end)
-  self.rooms[room.id] = room
-
-  room:connect(self:_build_endpoint(response.room.processId .. "/" .. room.id, {sessionId = room.sessionId}))
-end
-
-function client:_build_endpoint(path, options)
-  path = path or ""
-  options = options or {}
-
-  local params = {}
-  for k, v in pairs(options) do
-    table.insert(params, k .. "=" .. tostring(v))
-  end
-
-  return self.hostname .. path .. "?" .. table.concat(params, "&")
-end
-
-function client:_request(url, method, headers, body, callback)
-  http.request(url, method, function(self, id, response)
-		local data = response.response ~= '' and json.decode(response.response)
-    local has_error = (response.status >= 400)
-    local err = nil
-
-    if not data and response.status == 0 then
-      return callback("offline")
-    end
-
-    if has_error or data.error then
-      err = (not data or next(data) == nil) and response.response or data.error
-    end
-
-    callback(err, data)
-	end, headers, body or "", { timeout = Connection.config.connect_timeout })
-end
+---Join a room by manually consuming a "seat reservation".
+---@param reservation any
+---@param callback fun(err:string,room:Room):nil
+---@return nil
+function client:consume_seat_reservation(reservation, callback) end
 
 return client
--- colyseus-defold-0.14.1/colyseus/eventemitter.lua
+++ colyseus-defold-0.14.1/colyseus/eventemitter.lua
@@ -1,55 +1,35 @@
 --
 -- Based on https://github.com/wscherphof/lua-events/
 --
-
+---@class EventEmitter
 local EventEmitter = {}
 
-function table_find(tab,el)
-  for index, value in pairs(tab) do
-    if value == el then
-      return index
-    end
-  end
-end
+---Instantiate an EventEmitter.
+---@param object any
+---@return EventEmitter
+function EventEmitter:new(object) end
 
-function EventEmitter:new(object)
+---Listen to an event.
+---@param event string
+---@param listener fun(...)
+---@return fun(...)
+function EventEmitter:on (event, listener) end
 
-  object = object or {}
-  object._on = {}
+---Remove listener to an event.
+---@param event string
+---@param listener fun(...)
+---@return nil
+function EventEmitter:off (event, listener) end
 
-  function object:on (event, listener)
-    self._on[event] = self._on[event] or {}
-    table.insert(self._on[event], listener)
-    return listener
-  end
+---Get all the listeners to an event.
+---@param event string
+---@return table
+function EventEmitter:listeners (event) end
 
-  function object:off (event, listener)
-    if event then
-      if not listener then
-        table.remove(self._on[event])
-      else
-        table.remove(self._on[event], table_find(self._on[event], listener))
-      end
-    else
-      for event, listener in ipairs(self._on) do
-        self:off(event)
-      end
-    end
-  end
-
-  function object:listeners (event)
-    return self._on[event] or {}
-  end
-
-  function object:emit (event, ...)
-    for _, listener in ipairs(self:listeners(event)) do
-      if "function" == type(listener) then
-        listener(...)
-      end
-    end
-  end
-
-  return object
-end
+---Emit an event.
+---@param event string
+---@vararg any
+---@return nil
+function EventEmitter:emit (event, ...) end
 
 return EventEmitter
